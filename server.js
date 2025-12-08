a2c0bde4d8349f53707b1eaf922d7752e861136e
MarianaEib-24 <mariana.sankye@gmail.com>
2025-12-08 21:25:43 +0000

    Rename registration route to register

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-app.get('/registration', (req, res) => {
+app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'registration.html'));
 });
 
